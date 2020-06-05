7cb29f61f443a84d0a82e8e658f8f237d0e51ec4
janarild.eileng <janarild.eileng@fhi.no>
2020-06-05 15:03:22 +0200

    Hello

--- TestPatternConsoleApp/Program.cs
+++ TestPatternConsoleApp/Program.cs
@@ -6,7 +6,7 @@ namespace TestPatternConsoleApp
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello TestPatternConsoleApp!");
         }
     }
 }
